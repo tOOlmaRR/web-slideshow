8fdb1b289e2142370887ad16470cd5b22ccc7608
tOOlmaRR <georgeumarr@hotmail.com>
2020-06-22 17:58:54 -0400

    troubleshooting failing unit tests in the cloud
    echo out the array that I'm not expecting to receive as output

--- tests/WebSlideshow/WebSlideshowTest.php
+++ tests/WebSlideshow/WebSlideshowTest.php
@@ -57,7 +57,12 @@ final class WebSlideshowTest extends TestCase
         // invoke the function
         $photosReturned = $this->invokeMethod($slideshow, WebSlideshowTest::FUNCTION_NAME_DETERMINEPHOTOSTODISPLAYFORPATH, $inputs);
         $this->assertEmpty($photosReturned);
-        var_dump($photosReturned);
+        foreach($photosReturned as $key => $photo) {
+            print_r($photo);
+            foreach ($photo as $key => $value) {
+                echo $key . ' => ' . $value . PHP_EOL;
+            }
+        }
 
         // destroy test folders
         $this->destroyTestFilesAndFolders([$testPublicFolder_fullPath, $testPrivateFolder_fullPath]);
@@ -86,6 +91,12 @@ final class WebSlideshowTest extends TestCase
     //     // invoke the function
     //     $photosReturned = $this->invokeMethod($slideshow, WebSlideshowTest::FUNCTION_NAME_DETERMINEPHOTOSTODISPLAYFORPATH, $inputs);
     //     $this->assertNotEmpty($photosReturned);
+    //     foreach($photosReturned as $key => $photo) {
+    //         print_r($photo);
+    //         foreach ($photo as $key => $value) {
+    //             echo $key . ' => ' . $value . PHP_EOL;
+    //         }
+    //     }
 
     //     // destroy test folders
     //     $this->destroyTestFilesAndFolders([$testPublicFolder_fullPath, $testPrivateFolder_fullPath], [$testPhoto_fullPath]);
