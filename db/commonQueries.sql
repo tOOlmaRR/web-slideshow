d04a85e8ac809be036de2080aee5ab80bd66cd29
tOOlmaRR <georgeumarr@hotmail.com>
2021-05-22 17:55:14 -0400

    add common queries SQL script file
    common queries to retrieve images etc.

---
+++ db/commonQueries.sql
@@ -0,0 +1,13 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+select * from Images
+select * from Tags
+select * from TaggedImages
+
+select Images.FileName, Images.width, Images.height, Tags.Tag
+from Images
+join TaggedImages on Images.ImageID = TaggedImages.ImageID
+join Tags on TaggedImages.TagID = Tags.TagID
+
+--truncate table TaggedImages
+--truncate table Tags
+--truncate table Images
