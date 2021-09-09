f0803f771c2ca5608c4f0a39f733e6758beeff3f
tOOlmaRR <georgeumarr@hotmail.com>
2021-09-08 23:06:51 -0400

    add new page for db-driven slideshow
    includes a new stylesheet, a new slideshow class and a new config file. MUCH WORK NEEDED STILL

---
+++ scripts/dbMainConfig.php
@@ -0,0 +1,82 @@
+<?php
+$configuration = array();
+
+// Configure database connection
+$database = array();
+$database['type'] = 'mssql';
+$database['host'] = "MARR2\\GMARRMSSQL1";
+$database['name'] = "WebSlideshow-DEV";
+$database['user'] = "Urgele1";
+$database['password'] = "goldmOOn78!";
+$database['useSPROCS'] = true;
+
+    // Configure SPROC names
+    $database['SPROCS'] = array();
+    $database['SPROCS']['insert'] = array();
+    $database['SPROCS']['insert']['image'] = 'Image.Insert';
+    $database['SPROCS']['insert']['tag'] = 'Tag.Insert';
+    $database['SPROCS']['insert']['taggedImage'] = 'TaggedImage.Insert';
+
+    $database['SPROCS']['select']['image'] = 'Image.Select';
+    $database['SPROCS']['select']['tag'] = 'Tag.Select';
+
+$configuration["database"] = $database;
+
+// Configure public and private root paths
+/*$virtualRoots = array();
+$virtualRoots["public"] = "/myphotos/";
+$virtualRoots["private"] = "/myphotos/private/";
+$configuration["virtualRoots"] = $virtualRoots;
+
+$physicalRoots = array();
+$physicalRoots["public"] = "E:\\MyPhotos\\";
+$physicalRoots["private"] = "E:\\MyPhotos\\Private\\";
+$configuration["physicalRoots"] = $physicalRoots;
+
+
+// Configure All Slideshows
+$allSlideshows = array();
+$allSlideshows["Honeymoon"] = [
+    "name" => "Honeymoon",
+    "public" => false,
+    "physicalPath" => "Honeymoon\\",
+];
+$allSlideshows["Wedding"] = [
+    "name" => "Wedding",
+    "public" => true,
+    "physicalPath" => "Wedding\\",
+];
+$allSlideshows["WeddingAll"] = [
+    "name" => "Wedding - ALL",
+    "public" => true,
+    "physicalPaths" => [
+        "Wedding\\Disc1\\",
+        "Wedding\\Disc2\\"
+    ],
+    "includeSubfolders" => true
+];
+$configuration["allSlideshows"] = $allSlideshows;*/
+
+
+
+// determine chosen slideshow - use the first valid available slidehow by default
+/*$currentHourAndMinutes = date('Gi');
+if (isset($_GET) && isset($_GET["in"]) && ($_GET["in"] >= $currentHourAndMinutes - 1) && ($_GET["in"] <= $currentHourAndMinutes + 1)) {
+    foreach ($allSlideshows as $slideshow) {
+        if (!$slideshow["public"]) {
+            $chosenSlideshow = $slideshow;
+            break;
+        }
+    }
+} else {
+    foreach ($allSlideshows as $slideshow) {
+        if ($slideshow["public"]) {
+            $chosenSlideshow = $slideshow;
+            break;
+        }
+    }
+}
+
+if (isset($_POST) && isset($_POST["chosenSlideshow"])) {
+    $chosenSlideshow = $allSlideshows[$_POST["chosenSlideshow"]];
+}*/
---
+++ slideshow-db.php
@@ -0,0 +1,64 @@
+<?php
+    use toolmarr\WebSlideshow\DbWebSlideshow;
+
+    $maxHeight = 1000;
+    if (!isset($_GET['r'])) {
+        echo "<script language=\"JavaScript\">
+        <!--
+        let maxHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+        document.location=\"$_SERVER[PHP_SELF]?r=1&height=\" + maxHeight;
+        //-->
+        </script>";
+    }
+    else {
+        // Code to be displayed if resolution is detected
+        if (isset($_GET['height'])) {
+            // Resolution detected
+            $maxHeight = $_GET['height'];
+        }
+        
+        require_once('vendor/autoload.php');
+        require_once('scripts/dbMainConfig.php');
+        
+        // instantiate the Slideshow
+        $dbSlideshow = new DbWebSlideshow($maxHeight);
+    }
+?>
+<!DOCTYPE html>
+<html lang="en" xml:lang="en">
+    <head>
+        <title>JavaScript Slideshow v2</title>
+        <meta charset="utf-8">
+        <link href="styles/main-db.css" media="all" rel="Stylesheet" type="text/css" />
+        <!--<script type="text/javascript" language="javascript" src="scripts/slideshowFunctions.js"></script>-->
+    </head>
+    <body>
+        <div id="slideshowOptions">
+            <fieldset>
+                <legend>Slideshow Options:</legend>
+                <div class="slideshowSelection">             
+                    <?php $dbSlideshow->renderSlideshowTags($configuration) ?>
+                </div>
+                
+                <div class="randomizeToggle">
+                    <?php $dbSlideshow->renderRandomizeToggle() ?>
+                </div>
+
+                <div class="slideshowSpeed">
+                    <?php $dbSlideshow->renderSlideshowSpeed() ?>
+                </div>
+            </fieldset>
+        </div>
+        <!-- Slideshow container -->
+        <div class="slideshow-container">
+            <!-- Full-width images with number and caption text -->   
+
+            <!-- Next and previous buttons -->
+            <!--<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>-->
+            <a class="prev">&#10094;</a>
+            <a class="next">&#10095;</a>
+        </div>        
+        <!--script type="text/javascript" language="javascript">showSlides();</script>-->
+    </body>
+</html>
