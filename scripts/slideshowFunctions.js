ee63bdc32fef1028e8e8c7b96cf2b2d68a5b64e6
tOOlmaRR <georgeumarr@hotmail.com>
2021-11-21 11:19:04 -0500

    fix randomization
    fixed bug that was not including all slides in the randomized slideshow by adding an array shuffler method

--- scripts/slideshowFunctions.js
+++ scripts/slideshowFunctions.js
@@ -80,11 +80,7 @@ function randomize_change(checkbox)
 {
     clearInterval(slideShowIntervalID);
     if (checkbox.checked) {
-        slideIndexes = new Array();
-        for (let i = 0; i < slides.length; i++) {
-            const randomIndex = Math.floor(Math.random() * slides.length);
-            slideIndexes.push(randomIndex);
-        }
+        shuffleArray(slideIndexes);
     } else {
         slides = undefined;
         slideIndexes = undefined;
@@ -100,5 +96,16 @@ function haltSlideshow(checkbox) {
         const configuredIntervalText = document.getElementById("currentSlideshowSpeed").innerText;
         const configuredInterval = +configuredIntervalText * 1000;
         slideShowIntervalID = setTimeout(showSlides, configuredInterval);
+    }
+}
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+/* Source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
